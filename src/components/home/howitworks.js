a1fc69c6abcfa2921124bf23f2754fe6f7f48449
Temitope Olotin <olotintemitope@gmail.com>
2020-06-14 05:41:06 +0100

    fix how it works

--- src/components/home/howitworks.js
+++ src/components/home/howitworks.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardGroup } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { StyledCardGroup } from "./howitworks.style";
 
 import buyLogo from "../../../src/assets/images/buy.png";
@@ -7,43 +7,42 @@ import createLogo from "../../../src/assets/images/create.png";
 import browseLogo from "../../../src/assets/images/browse.png";
 
 export const HowItWorks = props => {
-  let cardGroup = (
-    <React.Fragment>
-      <StyledCardGroup>
-        <Card>
-          <Card.Img variant="top" src={browseLogo} />
-          <Card.Body>
-            <Card.Title>Browse</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={buyLogo} />
-          <Card.Body>
-            <Card.Title>Buy</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{" "}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={createLogo} />
-          <Card.Body>
-            <Card.Title>Create</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </StyledCardGroup>
-    </React.Fragment>
+  return (
+    <StyledCardGroup>
+      <Card>
+        <Card.Img variant="top" src={browseLogo} />
+        <Card.Body>
+          <Card.Title>Browse</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer. This is
+            a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.{" "}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="p-3">
+        <Card.Img variant="top" src={buyLogo} />
+        <Card.Body>
+          <Card.Title>Buy</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content. This card has supporting text below as a natural
+            lead-in to additional content.{" "}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={createLogo} />
+        <Card.Body>
+          <Card.Title>Create</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </StyledCardGroup>
   );
-  return cardGroup;
 };
--- src/components/home/howitworks.style.js
+++ src/components/home/howitworks.style.js
@@ -1,27 +1,56 @@
 import styled from "styled-components";
-import { CardDeck } from "react-bootstrap";
+import { CardColumns } from "react-bootstrap";
 
-export const StyledCardGroup = styled(CardDeck)`
+export const StyledCardGroup = styled(CardColumns)`
   border-radius: none;
-  display: flex;
-  justify-content: space-around;
-  font-family: "Avenir", serif;
 
   && .card {
     border: none;
-    margin-bottom: 20px;
+    margin-bottom: 2em;
+  }
+
+  && .card:nth-child(1),
+  .card:nth-child(2) {
+    margin-right: 2em;
   }
 
   && .card:nth-child(2) {
+    min-height: 35em;
+    margin-bottom: 2em;
   }
 
   && .card-title {
-    font-size: 18px;
+    font-size: 1.8em;
     font-weight: 700;
     text-align: center;
   }
 
-  @media screen and (min-width: 320px) {
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: baseline;
+  }
+
+  @media (min-width: 280px) and (max-width: 767px) {
+    display: flex;
     flex-direction: column;
+    -webkit-flex-direction: column;
+
+    && .card:nth-child(1),
+    .card:nth-child(2) {
+      margin-right: inherit;
+    }
+
+    && .card:nth-child(2) {
+      min-height: 35em;
+      margin-bottom: 2em;
+    }
+
+    && .card-title {
+      font-size: 1.5em;
+    }
+
+    && .p-3 {
+      padding: 0 !important;
+    }
   }
 `;
